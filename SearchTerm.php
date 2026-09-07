98961b5e236ff9b7762aa5949eca62320f5627f7
echkiee3 <hardionkingparone2005@gmail.com>
2026-09-07 23:20:02 +0800

    update

--- SearchTerm.php
+++ SearchTerm.php
@@ -7,7 +7,7 @@ $search_page = 'SearchTerm.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search Term</title>
-    <link rel="stylesheet" href="style.css?v=2">
+    <link rel="stylesheet" href="style.css?v=3">
 </head>
 <body>
     <?php include 'header.php'; ?>
--- Yearlevel.php
+++ Yearlevel.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Year</title>
-    <link rel="stylesheet" href="style.css?v=2">
+    <link rel="stylesheet" href="style.css?v=3">
 </head>
 <body>
     <?php include 'header.php'; ?>
