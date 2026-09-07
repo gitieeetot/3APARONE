fd6ee52868a74dd4940842d6294a2dd99277a64a
echkiee3 <hardionkingparone2005@gmail.com>
2026-09-08 00:25:42 +0800

    update

--- SearchCourse.php
+++ SearchCourse.php
@@ -1,5 +1,29 @@
-<?php include 'db_connect.php';
+<?php
+session_start();
+include 'db_connect.php';
 $search_page = 'SearchCourse.php';
+$message = "";
+
+if (isset($_POST['btn_update'])) {
+    $courseId = $_POST['course_id'];
+    $course = $_POST['course_value'];
+    $description = $_POST['course_description'];
+
+    $updateCourse = "UPDATE tbl_course SET course = '$course', description = '$description' WHERE ID = '$courseId'";
+    if ($conn->query($updateCourse)) {
+        $_POST['SearchCourse'] = $course;
+        $message = "Course successfully updated.";
+    }
+}
+
+if (isset($_POST['btn_delete'])) {
+    $courseId = $_SESSION['course_id'] ?? $_POST['course_id'];
+    $deleteCourse = "DELETE FROM tbl_course WHERE ID = '$courseId'";
+    if ($conn->query($deleteCourse)) {
+        $message = "Course successfully deleted.";
+        unset($_SESSION['course_id'], $_SESSION['course_value'], $_SESSION['course_description']);
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -26,6 +50,7 @@ $search_page = 'SearchCourse.php';
                 <input type="text" id="SearchCourse" name="SearchCourse" placeholder="Enter course here">
 
                 <button type="submit" name="btn_search">Search</button>
+                <?php echo $message; ?>
                 <?php
                 if (($_POST['SearchCourse'] ?? '') == "")
                 {
@@ -37,17 +62,33 @@ $search_page = 'SearchCourse.php';
                         {
                         while ($row = $resultCourse->fetch_assoc())
                         {
+                            $_SESSION['course_id'] = $row['ID'];
+                            $_SESSION['course_value'] = $row['COURSE'];
+                            $_SESSION['course_description'] = $row['DESCRIPTION'];
                             ?>
                             <div class="table">
                                 <div class="row">
+                                    <strong class="label">ID</strong>
+                                    <span class="value"><?php echo $row['ID']; ?></span>
+                                </div>
+                                <div class="row">
                                     <strong class="label">Course</strong>
-                                    <span class="value"><?php echo $row['COURSE']; ?></span>
+                                    <span class="value">
+                                        <input type="text" name="course_value" value="<?php echo $row['COURSE']; ?>" required>
+                                    </span>
                                 </div>
                                 <div class="row">
                                     <strong class="label">Description</strong>
-                                    <span class="value"><?php echo $row['DESCRIPTION']; ?></span>
+                                    <span class="value">
+                                        <input type="text" name="course_description" value="<?php echo $row['DESCRIPTION']; ?>" required>
+                                    </span>
                                 </div>
                             </div>
+                            <input type="hidden" name="course_id" value="<?php echo $row['ID']; ?>">
+                            <br>
+                            <button type="submit" name="btn_update">Update</button>
+                            <br>
+                            <button type="submit" name="btn_delete">Delete</button>
                             <?php
                         }
                     } else {
