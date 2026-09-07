5fdffd63c431fa6f8b85949f3f3195b12415dbd3
echkiee3 <hardionkingparone2005@gmail.com>
2026-09-07 23:04:03 +0800

    update

--- header.php
+++ header.php
@@ -6,12 +6,12 @@
     </div>
 
     <nav class="nav">
-        <a href="Course.php">Course</a>
-        <a href="Yearlevel.php">Year Level</a>
-        <a href="Term.php">Term</a>
-        <a href="Section.php">Section</a>
-        <a href="SchoolYear.php">School Year</a>
-        <a href="Subjects.php">Subjects</a>
+        <a href="Course.php" class="nav-link">Course</a>
+        <a href="Yearlevel.php" class="nav-link">Year Level</a>
+        <a href="Term.php" class="nav-link">Term</a>
+        <a href="Section.php" class="nav-link">Section</a>
+        <a href="SchoolYear.php" class="nav-link">School Year</a>
+        <a href="Subjects.php" class="nav-link">Subjects</a>
         <a href="<?php echo $search_page; ?>" class="search" aria-label="Search" title="Search">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="m21 21-4.34-4.34"/>
