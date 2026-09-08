8bff4f480fc17d6a79c1aeccac284e0db62bc86b
echkiee3 <hardionkingparone2005@gmail.com>
2026-09-08 22:38:23 +0800

    update

--- Course.php
+++ Course.php
@@ -1,9 +1,4 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-include 'db_connect.php';
+<?php include 'db_connect.php';
 $search_page = 'searchCourse.php';
 $success_message = "";
 $error_message = "";
