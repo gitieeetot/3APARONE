7c0fb0db0410b6d5b954732f735601b4ee6e1a2b
echkiee3 <hardionkingparone2005@gmail.com>
2026-09-08 21:12:49 +0800

    update

--- db_connect.php
+++ db_connect.php
@@ -1,20 +1,8 @@
 <?php
-if ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1') {
-    // for XAMPP
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $db_name = "3aparone";
-
-} else {
-    
-    // for INFINITYFREE
-    $host = "sql202.infinityfree.com";
-    $username = "if0_42844781";
-    $password = "CGPgHtY8k8N";
-    $db_name = "if0_42844781_db_3APARONE";
-}
-
+$host = "localhost";
+$username = "root";
+$password = "";
+$db_name = "3aparone";
 
 $conn = mysqli_connect($host, $username, $password, $db_name);
 
